60dd8958f88b2f7a85001bca9850cc19f9f309b0
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-20 18:01:51 -0300

    Parallel

--- practice-java/javaPractice/src/Main.java
+++ practice-java/javaPractice/src/Main.java
@@ -6,19 +6,30 @@ public class Main {
     public static void main(String[] args) {
         List<String> cities = new ArrayList<>();
         cities.addAll(Arrays.asList("London","New York","Tokio","Barcelona","Buenos Aires","Bogota","Ciudad de México","Lima"));
+
         for(int i = 0 ; i < cities.size(); i++){
             System.out.println(cities.get(i));
         }
+
         for(String city : cities){
             System.out.println(city);
         }
         //Con stream para recorrer
+        //expresion lambda*1
         cities.stream().forEach(city -> System.out.println(city));
-                                //expresion lambda*1
+        cities.forEach(city -> System.out.println(city));
 
+        //referencia de metodo.
         //si quiero pasar la funcion directamente
-        cities.stream().forEach(Main::printCity);
         // :: -> hace referencia al metodo.
+        cities.stream().forEach(Main::printCity);
+
+        //ejecucion en varios nucleos -> PARALLEL
+        //si el procesador tiene varios nucleos puedo usar el parallel para que ejecute en varios hilos
+        cities.stream().parallel().forEach(city -> System.out.println(city));
+        //separa la lista endiferentes lotes para procesarlos y luego los procesa esto logra que se procecen mas rapido
+        //pero el enlistado sera diferente.
+
 
         System.out.println("Hello world!");
     }
