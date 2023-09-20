d262be07cfb5c32eb6177060ee70a229c5607fc0
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-20 16:52:10 -0300

    Strating java practice

--- practice-java/javaPractice/src/Main.java
+++ practice-java/javaPractice/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<String> cities = new ArrayList<>();
+        cities.addAll(Arrays.asList("London","New York","Tokio","Barcelona","Buenos Aires","Bogota","Ciudad de México","Lima"));
+        for(int i = 0 ; i < cities.size(); i++){
+            System.out.println(cities.get(i));
+        }
+        for(String city : cities){
+            System.out.println(city);
+        }
+
         System.out.println("Hello world!");
     }
 }
