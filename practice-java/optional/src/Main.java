34cda6159daa27a6f1db4286b5dca8677267267c
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-21 17:35:59 -0300

    Opcional Excamples ok

--- practice-java/optional/src/Main.java
+++ practice-java/optional/src/Main.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        //Objeto Opcional puede ser valor null o Double
-        Optional<Double> result = averageScores();
+    List<String> countries = new ArrayList<>();
+    countries.addAll(Arrays.asList("United States","Argentina","Spain","Italy","Colombia","México","Ecuador","Peru"));
+    //findFirst devuelve un Optional
+    Optional<String> country =  countries.stream()
+            .filter(c->c.startsWith("Arg")).findFirst();
+    country.ifPresent(System.out::println);
+    }
+    public static void excampleOptional(Double ...scores){
+        Optional<Double> result = averageScores(8.0,8.0,7.0,8.0,9.0);
         if(result.isPresent()){
             System.out.println(result.get());
         }
@@ -12,6 +22,7 @@ public class Main {
         }
     }
     public static Optional<Double> averageScores(Double ...scores){
+        //Objeto Opcional puede ser valor null o Double
         if(scores.length == 0){
             return Optional.empty();
         }
