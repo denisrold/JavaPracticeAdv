76f5319c474f066c92fe9959a7f2aa0648797268
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-21 12:45:48 -0300

    WebScraper ok

--- practice-java/web-scraper/src/Main.java
+++ practice-java/web-scraper/src/Main.java
@@ -1,6 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
-    //Download Webs
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.stream.Collectors;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+    //Download Webs
+    //start url
+    String link = "https://www.bbc.com/";
+    //objectUrl
+        URL url = new URL(link);
+    //Conexión:
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    //download header content
+    String encoding = conn.getContentEncoding();
+    //download information
+        InputStream input = conn.getInputStream();
+        String result = new BufferedReader(new InputStreamReader(input))
+                .lines().collect(Collectors.joining());
+        System.out.println(result);
     }
 }
